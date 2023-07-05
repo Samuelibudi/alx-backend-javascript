fd25dcd320f7e16699d4bf1a824cbe8a17026e8a
Samuelibudi <ibudisamuel@gmail.com>
2023-07-05 12:59:40 +0000

    Adding files

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,40 +1,88 @@
 interface Student {
-	firstName: string;
-	lastName: string;
-	age: number;
-	location: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
 }
 
-const student1: Student = {
-	firstName: "John",
-	lastName: "Doe",
-	age: 20,
-	location: "New York"
-
+const Student1: Student = {
+  firstName: 'Samuel',
+  lastName: 'Ibudi',
+  age: 30,
+  location: 'Kampala',
 };
 
-const student2: Student = {
-	firstName: "Jane",
-	lastName: "Smith",
-	age: 22,
-	location: "Los Angeles"
+const Student2: Student = {
+  firstName: 'Shenin',
+  lastName: 'Kihembo',
+  age: 27,
+  location: 'Mbarara',
 };
 
-const studentsList: Student[] = [student1, student2];
-const table = document.createElement("table");
+const studentsList: Student[] = [Student1, Student2];
 
-studentsList.forEach(student => {
-	const row = document.createElement("tr");
+const styleSheet = `
+* {
+    margin: 0;
+    box-sizing: border-box;
+}
+body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 80%;
+    margin: 10%;
+}
+table {
+    border-collapse: collapse;
+}
+thead {
+    font-weight: bold;
+    background-color: blue;
+    color: white;
+}
+td {
+    padding: 10px;
+    border: 1px solid gray;
+    cursor: pointer;
+}
+td:hover {
+    background: gainsboro;
+}
+td:nth-child(1) {
+    text-align: center;
+}
+tbody tr:nth-child(even) {
+    background-color: #f4f4f4;
+}
+`;
 
-	const firstNameCell = document.createElement("td");
-	firstNameCell.textContent = student.firstName;
-	row.appendChild(firstNameCell);
+export const showStudent = (students: Array<Student>): void => {
+  const body = document.querySelector('body');
 
-	const locationCell = document.createElement("td");
-	locationCell.textContent = student.location;
-	row.appendChild(locationCell);
+  const table = document.createElement('table');
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
+  const headTr = document.createElement('tr');
 
-	table.appendChild(row);
-});
+  headTr.innerHTML = '<td>Name</td> <td>Location</td>';
 
-document.body.appendChild(table);
+  thead.appendChild(headTr);
+
+  for (const student of students) {
+    const bodyTr = document.createElement('tr');
+    bodyTr.innerHTML = `<td>${student.firstName}</td> <td>${student.location}</td>`;
+    tbody.appendChild(bodyTr);
+  }
+
+  table.appendChild(thead);
+  table.appendChild(tbody);
+  document.body.appendChild(style);
+  document.body.appendChild(table);
+};
+
+showStudent(studentsList);
+document.title = 'Task 0';
+const style = document.createElement('style');
+style.innerHTML = styleSheet;
+document.head.appendChild(style);
